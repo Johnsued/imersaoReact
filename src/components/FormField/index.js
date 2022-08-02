16cca464ecb24d6530e0c6504fe1498c5fec4e07
John Soares <johnsued25@gmail.com>
2022-08-02 08:31:36 -0300

    still not end

--- src/components/FormField/index.js
+++ src/components/FormField/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
 function FormField({ label, type, name, value, onChange }) {
+
+   
     return (
         <div>
             <label>
                 {label}
+            
                 <input
                     type={type}
                     value={value}
